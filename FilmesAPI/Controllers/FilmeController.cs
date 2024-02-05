944bdab72810843d392b5bc3c07a14b9beba451b
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2024-02-05 20:33:22 -0300

    Finalizado aula 3

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -7,13 +7,28 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new();
+    private static readonly List<Filme> filmes = new();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
+        return CreatedAtAction(nameof(RecuperaFilmePorID), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorID(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,17 +4,19 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O título do filme é obrigatório")]
-    public string Titulo { get; set; }
+    public string? Titulo { get; set; }
 
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
-    public string Genero { get; set; }
+    public string? Genero { get; set; }
 
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 
     [Required(ErrorMessage = "O nome do diretor é obrigatório")]
-    public string Diretor { get; set; }
+    public string? Diretor { get; set; }
 }
